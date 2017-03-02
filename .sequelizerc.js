dd5bddd4f7d2483e3562c4afda74ba5177f75500
Роман <roman.sarder@yandex.ru>
2017-03-02 17:23:15 +0200

    initialize sequelizerc install pg, sequelize, pg-hstore packages init sequelize refactor models/index.js into ES6 style create todos db update config to use local db

---
+++ .sequelizerc.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+
+module.exports = {
+    //app settings file
+    "config": path.resolve('./server/config', 'config.json'),
+    //app models
+    "models-path": path.resolve('./server/models'),
+    //folder containing seed data
+    "seeders-path": path.resolve('./server/seeders'),
+    //app migrations
+    "migrations-path": path.resolve('./server/migrations')
+};
